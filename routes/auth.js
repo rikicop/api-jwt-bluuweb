ac822a3fdd83adc4b83f79b1bc5b511a759b7dad
rikicop <ruperto1@protonmail.com>
2021-11-06 17:47:22 -0400

    token 3

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,49 @@ const router = require("express").Router();
 
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const Joi = require("@hapi/joi");
 
 const schemaRegister = Joi.object({
   name: Joi.string().min(6).max(255).required(),
   email: Joi.string().min(6).max(255).required().email(),
   password: Joi.string().min(6).max(1024).required(),
+});
+
+const schemaLogin = Joi.object({
+  email: Joi.string().min(6).max(255).required().email(),
+  password: Joi.string().min(6).max(1024).required(),
+});
+
+router.post("/login", async (req, res) => {
+  const { error } = schemaLogin.validate(req.body);
+  if (error) return res.status(400).json({ error: error.details[0].message });
+
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).json({ error: "Usuario no encontrado" });
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword)
+    return res.status(400).json({ error: "contraseña no válida" });
+
+  const token = jwt.sign(
+    {
+      name: user.name,
+      id: user._id,
+    },
+    process.env.TOKEN_SECRET
+  );
+
+  res.header("auth-token", token).json({
+    error: null,
+    data: { token },
+  });
+
+  /*  res.json({
+    error: null,
+    data: "exito bienvenido",
+    token,
+  }); */
 });
 
 router.post("/register", async (req, res) => {
